29b9499b5e7f5bd7afb7ecd9d5b246df09f4d5e8
Prasanna Pratap Singh <prap.sin.25@gmail.com>
2026-01-01 17:58:19 +0530

    Change axios base URL to production endpoint

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://compilex-backend-ae28.onrender.com",
     headers:{
         'Content-Type':'application/json'
     }
